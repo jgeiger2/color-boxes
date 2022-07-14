6be1c825e151c80f80dc8d2086766c26da4f1792
James Geiger <jgeiger2@me.com>
2022-07-14 14:54:48 -0400

    initial commit

---
+++ src/BoxContainer.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import './BoxContainer.css'
+
+class BoxContainer extends Component {
+    static defaultProps = {
+        numBoxes: 18
+    }
+    render() {
+        const boxes = Array.from({length: this.props.numBoxes}).map(
+            () => <Box />
+        )
+        return  (
+            <>
+            <div><h1>Click on the boxes</h1></div>
+            <div className='BoxContainer'>
+                {boxes}
+            </div>
+            </>
+        )
+    }
+}
+
+export default BoxContainer
